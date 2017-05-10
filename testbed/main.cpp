3bb1bff5526a829a7c03bdd3fd1898646e92ad0b
xu2201g <xu2201g@gmail.com>
2017-05-10 20:01:22 +0200

    created a circle and rendered it to the window

--- testbed/main.cpp
+++ testbed/main.cpp
@@ -1,16 +1,33 @@
-//#include <iostream>
-
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv)
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Window");
+	unsigned int windowWidth = 640;
+	unsigned int windowHeight = 480;
 
+	//create sfml window 
+	sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML Window");
+
+	//create a cicleshape 
+	float radius = 100.0f;
+	sf::CircleShape circle(radius);
+	circle.setFillColor(sf::Color::Cyan);
+
+	//top left corner of the cicle area
+	//circle.setPosition(100, 100);
+
+	//set origin at the center of the cicle
+	circle.setOrigin(circle.getRadius(), circle.getRadius());
+	circle.setPosition(100, 100);
+
+	//window rendering loop
 	while (window.isOpen())
 	{
+		//process all window events by polling one after another
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			//close the window if we get a close event
 			if (event.type == sf::Event::Closed)
 			{
 				//close window button clicked
@@ -18,13 +35,14 @@ int main(int argc, char** argv)
 			}
 		}
 
+		//clear the window
 		window.clear(sf::Color::Black);
 
-		//draw 
+		//draw something here
+		window.draw(circle);
 
+		//display on screen what has been rendered to the window so far
 		window.display();
 	}
 
-	//std::cout << "Hello World!" << std::endl;
-	system("pause");
 }
