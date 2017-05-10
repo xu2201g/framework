541873286801e0fb7455fe3e90ff302da64c27bf
xu2201g <xu2201g@gmail.com>
2017-05-10 19:20:29 +0200

    sfml setup and created simple window

--- testbed/main.cpp
+++ testbed/main.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+//#include <iostream>
+
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Window");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				//close window button clicked
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color::Black);
+
+		//draw 
+
+		window.display();
+	}
+
+	//std::cout << "Hello World!" << std::endl;
 	system("pause");
 }
